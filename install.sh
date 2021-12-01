3bf4036d97e22cbc551945afdf452a86c3781a05
fra <fra.sette.p.c@gmail.com>
2021-12-01 09:19:02 +0100

    installer

--- install.sh
+++ install.sh
@@ -20,11 +20,16 @@ echo "I hope you're using Linux"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+cd ~
+
 curl -sL install-node.vercel.app/lts | bash
+
+cd ~
 
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
 
+cd ~
 
 cd .config
 
