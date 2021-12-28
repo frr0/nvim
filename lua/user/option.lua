862610072c27246bdfd93486db2d6e30db8e523c
fra <fra.sette.p.c@gmail.com>
2021-12-28 10:36:15 +0100

    new after reset prev hard reset on a #285d0e6e

---
+++ lua/user/option.lua
@@ -0,0 +1,25 @@
+local options = {
+  number = true,                           -- set numbered lines
+  relativenumber = false,                  -- set relative numbered lines
+  mouse = "a",                             -- allow the mouse to be used in neovim
+  hidden = true,                           --
+  expandtab = true,                        -- convert tabs to spaces
+  shiftwidth = 2,                          -- the number of spaces inserted for each indentation
+  tabstop = 2,                             -- insert 2 spaces for a tab
+  cursorline = true,                       -- highlight the current line
+  autoindent = true,                       -- autoindent
+  ignorecase = true
+  }
+
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+
+vim.cmd "colorscheme onedark"
+--vim.cmd "cset t_Co=256"
+--vim.cmd "cset background=dark"
+vim.cmd "hi Normal ctermbg=16 guibg=#292C32"
+vim.cmd "hi LineNr ctermbg=16 guibg=#292C32"
+vim.cmd "syntax on"
+
