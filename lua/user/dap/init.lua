8e7d51069b1e63100dc6c08792fb8afa8f77d11c
fra <fra.sette.p.c@gmail.com>
2022-01-10 18:58:13 +0100

    j

--- lua/user/dap/init.lua
+++ lua/user/dap/init.lua
@@ -1,163 +1,187 @@
-#!/usr/bin/env lua
-
-local dap = require('dap')
-
-dap.adapters.cppdbg = {
-  type = 'executable',
-  command = '.local/share/nvimvimplug/vimspector/gadgets/linux/download/vscode-cpptools/1.6.0/root/extension/debugAdapters/bin/OpenDebugAD7',
-}
-
---local dap = require('dap')
-dap.configurations.cpp = {
+local dap = require "dap"
+dap.configurations.java = {
   {
-    name = "Launch file",
-    type = "cppdbg",
-    request = "launch",
-    program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-    cwd = '${workspaceFolder}',
-    stopOnEntry = true,
-  },
-  {
-    name = 'Attach to gdbserver :1234',
-    type = 'cppdbg',
-    request = 'launch',
-    MIMode = 'gdb',
-    miDebuggerServerAddress = 'localhost:1234',
-    miDebuggerPath = '/usr/bin/gdb',
-    cwd = '${workspaceFolder}',
-    program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
+    type = "java";
+    request = "attach";
+    name = "Debug (Attach) - Remote";
+    hostName = "127.0.0.1";
+    port = 5005;
   },
 }
 
-dap.adapters.go = function(callback, config)
-  local stdout = vim.loop.new_pipe(false)
-  local handle
-  local pid_or_err
-  local port = 38697
-  local opts = {
-    stdio = {nil, stdout},
-    args = {"dap", "-l", "127.0.0.1:" .. port},
-    detached = true
-  }
-  handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
-    stdout:close()
-    handle:close()
-    if code ~= 0 then
-      print('dlv exited with code', code)
-    end
-  end)
-  assert(handle, 'Error running dlv: ' .. tostring(pid_or_err))
-  stdout:read_start(function(err, chunk)
-    assert(not err, err)
-    if chunk then
-      vim.schedule(function()
-        require('dap.repl').append(chunk)
-      end)
-    end
-  end)
-  -- Wait for delve to start
-  vim.defer_fn(
-    function()
-      callback({type = "server", host = "127.0.0.1", port = port})
-    end,
-    100)
-end
+--local map = require("utils").map
 
--- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
-dap.configurations.go = {
-  {
-    type = "go",
-    name = "Debug",
-    request = "launch",
-    program = "${file}"
-  },
-  {
-    type = "go",
-    name = "Debug test", -- configuration for debugging test files
-    request = "launch",
-    mode = "test",
-    program = "${file}"
-  },
-  -- works with go.mod packages and sub packages
-  {
-    type = "go",
-    name = "Debug test (go.mod)",
-    request = "launch",
-    mode = "test",
-    program = "./${relativeFileDirname}"
-  }
-}
-
-require("nvim-dap-virtual-text").setup {
-    enabled = true,                     -- enable this plugin (the default)
-    enabled_commands = true,            -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
-    highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-    highlight_new_as_changed = false,   -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
-    show_stop_reason = true,            -- show stop reason when stopped for exceptions
-    commented = false,                  -- prefix virtual text with comment string
-    -- experimental features:
-    virt_text_pos = 'eol',              -- position of virtual text, see `:h nvim_buf_set_extmark()`
-    all_frames = false,                 -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
-    virt_lines = false,                 -- show virtual lines instead of virtual text (will flicker!)
-    virt_text_win_col = nil             -- position the virtual text at a fixed window column (starting from the first text column) ,
-                                        -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
-}
-
-require("dapui").setup({
-  icons = { expanded = "▾", collapsed = "▸" },
-  mappings = {
-    -- Use a table to apply multiple mappings
-    expand = { "<CR>", "<2-LeftMouse>" },
-    open = "o",
-    remove = "d",
-    edit = "e",
-    repl = "r",
-  },
-  sidebar = {
-    -- open_on_start = true,
-    -- You can change the order of elements in the sidebar
-    elements = {
-      -- Provide as ID strings or tables with "id" and "size" keys
-      {
-        id = "scopes",
-        size = 0.25, -- Can be float or integer > 1
-      },
-      { id = "breakpoints", size = 0.25 },
-      { id = "stacks", size = 0.25 },
-      { id = "watches", size = 00.25 },
-    },
-    size = 40,
-    position = "left", -- Can be "left", "right", "top", "bottom"
-  },
-  tray = {
-    -- open_on_start = true,
-    elements = { "repl" },
-    size = 10,
-    position = "bottom", -- Can be "left", "right", "top", "bottom"
-  },
-  floating = {
-    max_height = nil, -- These can be integers or a float between 0 and 1.
-    max_width = nil, -- Floats will be treated as percentage of your screen.
-    mappings = {
-      close = { "q", "<Esc>" },
-    },
-  },
-  windows = { indent = 1 },
-})
+--[[map("n", "<F5>", ":lua require'dap'.continue()<CR>")]]
+--[[map("n", "<F10>", ":lua require'dap'.step_over()<CR>")]]
+--[[map("n", "<F11>", ":lua require'dap'.step_into()<CR>")]]
+--[[map("n", "<F12>", ":lua require'dap'.step_out()<CR>")]]
+--[[map("n", "<Leader>b", ":lua require'dap'.toggle_breakpoint()<CR>")]]
+--[[map("n", "<Leader>B", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")]]
+--[[map("n", "<Leader>lp", ":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>")]]
+--[[map("n", "<Leader>dr", ":lua require'dap'.repl.toggle()<CR>")]]
+--[[map("n", "<Leader>dl", ":lua require'dap'.run_last()<CR>")]]
 
 
-vim.api.nvim_command([[
-  nnoremap <silent> <F5> :lua require'dap'.continue()<CR>
-  nnoremap <slent> <F10> :lua require'dap'.step_over()<CR>
-  nnoremap <silent> <F11> :lua require'dap'.step_into()<CR>
-  nnoremap <silent> <F12> :lua require'dap'.step_out()<CR>
-  nnoremap <silent> <leader>b :lua require'dap'.toggle_breakpoint()<CR>
-  nnoremap <silent> <leader>B :lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>
-  nnoremap <silent> <leader>lp :lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>
-  nnoremap <silent> <leader>dr :lua require'dap'.repl.open()<CR>
-  nnoremap <silent> <leader>dl :lua require'dap'.run_last()<CR>
-]])
+--[[#!/usr/bin/env lua]]
+
+--[[local dap = require('dap')]]
+
+--[[dap.adapters.cppdbg = {]]
+  --[[type = 'executable',]]
+  --[[command = '.local/share/nvimvimplug/vimspector/gadgets/linux/download/vscode-cpptools/1.6.0/root/extension/debugAdapters/bin/OpenDebugAD7',]]
+--[[}]]
+
+--[[--local dap = require('dap')]]
+--[[dap.configurations.cpp = {]]
+  --[[{]]
+    --[[name = "Launch file",]]
+    --[[type = "cppdbg",]]
+    --[[request = "launch",]]
+    --[[program = function()]]
+      --[[return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')]]
+    --[[end,]]
+    --[[cwd = '${workspaceFolder}',]]
+    --[[stopOnEntry = true,]]
+  --[[},]]
+  --[[{]]
+    --[[name = 'Attach to gdbserver :1234',]]
+    --[[type = 'cppdbg',]]
+    --[[request = 'launch',]]
+    --[[MIMode = 'gdb',]]
+    --[[miDebuggerServerAddress = 'localhost:1234',]]
+    --[[miDebuggerPath = '/usr/bin/gdb',]]
+    --[[cwd = '${workspaceFolder}',]]
+    --[[program = function()]]
+      --[[return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')]]
+    --[[end,]]
+  --[[},]]
+--[[}]]
+
+--[[dap.adapters.go = function(callback, config)]]
+  --[[local stdout = vim.loop.new_pipe(false)]]
+  --[[local handle]]
+  --[[local pid_or_err]]
+  --[[local port = 38697]]
+  --[[local opts = {]]
+    --[[stdio = {nil, stdout},]]
+    --[[args = {"dap", "-l", "127.0.0.1:" .. port},]]
+    --[[detached = true]]
+  --[[}]]
+  --[[handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)]]
+    --[[stdout:close()]]
+    --[[handle:close()]]
+    --[[if code ~= 0 then]]
+      --[[print('dlv exited with code', code)]]
+    --[[end]]
+  --[[end)]]
+  --[[assert(handle, 'Error running dlv: ' .. tostring(pid_or_err))]]
+  --[[stdout:read_start(function(err, chunk)]]
+    --[[assert(not err, err)]]
+    --[[if chunk then]]
+      --[[vim.schedule(function()]]
+        --[[require('dap.repl').append(chunk)]]
+      --[[end)]]
+    --[[end]]
+  --[[end)]]
+  --[[-- Wait for delve to start]]
+  --[[vim.defer_fn(]]
+    --[[function()]]
+      --[[callback({type = "server", host = "127.0.0.1", port = port})]]
+    --[[end,]]
+    --[[100)]]
+--[[end]]
+
+--[[-- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md]]
+--[[dap.configurations.go = {]]
+  --[[{]]
+    --[[type = "go",]]
+    --[[name = "Debug",]]
+    --[[request = "launch",]]
+    --[[program = "${file}"]]
+  --[[},]]
+  --[[{]]
+    --[[type = "go",]]
+    --[[name = "Debug test", -- configuration for debugging test files]]
+    --[[request = "launch",]]
+    --[[mode = "test",]]
+    --[[program = "${file}"]]
+  --[[},]]
+  --[[-- works with go.mod packages and sub packages]]
+  --[[{]]
+    --[[type = "go",]]
+    --[[name = "Debug test (go.mod)",]]
+    --[[request = "launch",]]
+    --[[mode = "test",]]
+    --[[program = "./${relativeFileDirname}"]]
+  --[[}]]
+--[[}]]
+
+--[[require("nvim-dap-virtual-text").setup {]]
+    --[[enabled = true,                     -- enable this plugin (the default)]]
+    --[[enabled_commands = true,            -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)]]
+    --[[highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText]]
+    --[[highlight_new_as_changed = false,   -- highlight new variables in the same way as changed variables (if highlight_changed_variables)]]
+    --[[show_stop_reason = true,            -- show stop reason when stopped for exceptions]]
+    --[[commented = false,                  -- prefix virtual text with comment string]]
+    --[[-- experimental features:]]
+    --[[virt_text_pos = 'eol',              -- position of virtual text, see `:h nvim_buf_set_extmark()`]]
+    --[[all_frames = false,                 -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.]]
+    --[[virt_lines = false,                 -- show virtual lines instead of virtual text (will flicker!)]]
+    --[[virt_text_win_col = nil             -- position the virtual text at a fixed window column (starting from the first text column) ,]]
+                                        --[[-- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`]]
+--[[}]]
+
+--[[require("dapui").setup({]]
+  --[[icons = { expanded = "▾", collapsed = "▸" },]]
+  --[[mappings = {]]
+    --[[-- Use a table to apply multiple mappings]]
+    --[[expand = { "<CR>", "<2-LeftMouse>" },]]
+    --[[open = "o",]]
+    --[[remove = "d",]]
+    --[[edit = "e",]]
+    --[[repl = "r",]]
+  --[[},]]
+  --[[sidebar = {]]
+    --[[-- open_on_start = true,]]
+    --[[-- You can change the order of elements in the sidebar]]
+    --[[elements = {]]
+      --[[-- Provide as ID strings or tables with "id" and "size" keys]]
+      --[[{]]
+        --[[id = "scopes",]]
+        --[[size = 0.25, -- Can be float or integer > 1]]
+      --[[},]]
+      --[[{ id = "breakpoints", size = 0.25 },]]
+      --[[{ id = "stacks", size = 0.25 },]]
+      --[[{ id = "watches", size = 00.25 },]]
+    --[[},]]
+    --[[size = 40,]]
+    --[[position = "left", -- Can be "left", "right", "top", "bottom"]]
+  --[[},]]
+  --[[tray = {]]
+    --[[-- open_on_start = true,]]
+    --[[elements = { "repl" },]]
+    --[[size = 10,]]
+    --[[position = "bottom", -- Can be "left", "right", "top", "bottom"]]
+  --[[},]]
+  --[[floating = {]]
+    --[[max_height = nil, -- These can be integers or a float between 0 and 1.]]
+    --[[max_width = nil, -- Floats will be treated as percentage of your screen.]]
+    --[[mappings = {]]
+      --[[close = { "q", "<Esc>" },]]
+    --[[},]]
+  --[[},]]
+  --[[windows = { indent = 1 },]]
+--[[})]]
+
+
+--[[vim.api.nvim_command([[]]
+  --[[nnoremap <silent> <F5> :lua require'dap'.continue()<CR>]]
+  --[[nnoremap <slent> <F10> :lua require'dap'.step_over()<CR>]]
+  --[[nnoremap <silent> <F11> :lua require'dap'.step_into()<CR>]]
+  --[[nnoremap <silent> <F12> :lua require'dap'.step_out()<CR>]]
+  --[[nnoremap <silent> <leader>b :lua require'dap'.toggle_breakpoint()<CR>]]
+  --[[nnoremap <silent> <leader>B :lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>]]
+  --[[nnoremap <silent> <leader>lp :lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>]]
+  --[[nnoremap <silent> <leader>dr :lua require'dap'.repl.open()<CR>]]
+  --[[nnoremap <silent> <leader>dl :lua require'dap'.run_last()<CR>]]
+--[[<])]]
