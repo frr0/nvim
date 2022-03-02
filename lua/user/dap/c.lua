fb163bb35ed4751a51206f18ae8b326603c7c695
fra <fra.sette.p.c@gmail.com>
2022-03-02 16:34:40 +0100

    dap2

--- lua/user/dap/c.lua
+++ lua/user/dap/c.lua
@@ -5,6 +5,26 @@ if not dap_status then
 	return
 end
 
+dap.adapters.cppdbg = {
+  type = 'executable',
+  command = '~/.local/share/nvim/dapinstall/ccppr_vsc/cpptools-linux/extension/bin/OpenDebugAD7',
+}
+
+--dap.configurations.cpp = {
+  --{
+    --name = 'Attach to gdbserver',
+    --type = 'cppdbg',
+    --request = 'launch',
+    --MIMode = 'gdb',
+    --miDebuggerServerAddress = 'localhost:1234',
+    --miDebuggerPath = '/usr/bin/gdb',
+    --cwd = '${workspaceFolder}',
+    --program = function()
+      --return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    --end,
+  --},
+--}
+
 --dap.adapters.lldb = {
 	--type = "executable",
 	--attach = { pidProperty = "pid", pidSelect = "ask" },
@@ -43,46 +63,46 @@ end
 --dap.configurations.rust = dap.configurations.cpp
 
 -- adapter definition
-dap.adapters.cppdbg = {
-        id = "cppdbg",
-        type = "executable",
-        --command = {os.getenv("HOME") .. "/.vscode-server/extensions/ms-vscode.cpptools-1.8.4/debugAdapters/bin/OpenDebugAD7"},
-        command = "~/.local/share/nvim/dapinstall/ccppr_vsc/cpptools-linux/extension/bin/OpenDebugAD7",
-        --command = {os.getenv("HOME") .. "/.local/share/nvim/dapinstall/ccppr_vsc/cpptools-linux/extension/bin/OpenDebugAD7"},
-}
+--dap.adapters.cppdbg = {
+        --id = "cppdbg",
+        --type = "executable",
+        ----command = {os.getenv("HOME") .. "/.vscode-server/extensions/ms-vscode.cpptools-1.8.4/debugAdapters/bin/OpenDebugAD7"},
+        --command = "~/.local/share/nvim/dapinstall/ccppr_vsc/cpptools-linux/extension/bin/OpenDebugAD7",
+        ----command = {os.getenv("HOME") .. "/.local/share/nvim/dapinstall/ccppr_vsc/cpptools-linux/extension/bin/OpenDebugAD7"},
+--}
 
-dap.configurations.cpp = {
-        {
-                name = "(gdb) Launch file",
-                type = "cppdbg",
-                request = "launch",
-                program = function()
-                        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-                end,
-                cwd = "${workspaceFolder}",
-                stopOnEntry = true,
-                setupCommands = {
-                        {
-                                text = "-enable-pretty-printing",
-                                description = "enable pretty printing",
-                                ignoreFailures = false,
-                        },
-                },
-                -- MIMode = "gdb",
-                -- miDebuggerPath = "/usr/bin/gdb",
-        },
-        -- {
-        --      name = "Attach to gdbserver :1234",
-        --      type = "cppdbg",
-        --      request = "launch",
-        --      MIMode = "gdb",
-        --      miDebuggerServerAddress = "localhost:1234",
-        --      miDebuggerPath = "/usr/bin/gdb",
-        --      cwd = "${workspaceFolder}",
-        --      program = function()
-        --              return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-        --      end,
-        -- },
-}
+--dap.configurations.cpp = {
+        --{
+                --name = "(gdb) Launch file",
+                --type = "cppdbg",
+                --request = "launch",
+                --program = function()
+                        --return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                --end,
+                --cwd = "${workspaceFolder}",
+                --stopOnEntry = true,
+                --setupCommands = {
+                        --{
+                                --text = "-enable-pretty-printing",
+                                --description = "enable pretty printing",
+                                --ignoreFailures = false,
+                        --},
+                --},
+                ---- MIMode = "gdb",
+                ---- miDebuggerPath = "/usr/bin/gdb",
+        --},
+        ---- {
+        ----      name = "Attach to gdbserver :1234",
+        ----      type = "cppdbg",
+        ----      request = "launch",
+        ----      MIMode = "gdb",
+        ----      miDebuggerServerAddress = "localhost:1234",
+        ----      miDebuggerPath = "/usr/bin/gdb",
+        ----      cwd = "${workspaceFolder}",
+        ----      program = function()
+        ----              return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        ----      end,
+        ---- },
+--}
 
-dap.configurations.c = dap.configurations.cpp
+--dap.configurations.c = dap.configurations.cpp
